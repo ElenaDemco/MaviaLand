37c21bed52e3854b998c8779ebb9480fbc28704f
Elena <elena.demco@gmail.com>
2023-05-07 21:34:26 +0200

    Creating a class

--- MaviaLand/ViewControllers/ViewController.swift
+++ MaviaLand/ViewControllers/ViewController.swift
@@ -8,9 +8,31 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchToken()
+    }
+}
+
+// MARK: - Networking
+private extension ViewController {
+    func fetchToken() {
+        URLSession.shared.dataTask(with: URL(string: "https://api.opensea.io/collection/mavia-land?format=json")!) { data, response, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "Error")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let maviaLand = try decoder.decode(MaviaLand.self, from: data)
+                print(maviaLand.collection.information)
+            } catch {
+                print(error)
+            }
+        }.resume()
     }
 }
 
