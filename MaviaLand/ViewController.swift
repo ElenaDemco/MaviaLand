fcdf67a494c34004f9283e13c553f73a82f8ef5d
Elena <elena.demco@gmail.com>
2023-05-07 19:09:14 +0200

    Initial Commit

---
+++ MaviaLand/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MaviaLand
+//
+//  Created by Yelena Demchenko on 07.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
