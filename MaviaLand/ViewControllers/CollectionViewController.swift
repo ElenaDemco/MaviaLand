e90830f01742441d0b9e170f831f3342cbbc9ffd
Elena <elena.demco@gmail.com>
2023-05-09 18:12:37 +0200

    Added class for TableView

---
+++ MaviaLand/ViewControllers/CollectionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewController.swift
+//  MaviaLand
+//
+//  Created by Yelena Demchenko on 09.05.2023.
+//
+
+import UIKit
+
+class CollectionViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
